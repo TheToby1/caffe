2937569320e8cae6d8fdfcba14a0ed2e24c6332e
Toby Burns <toby8urns@gmail.com>
2019-04-27 15:01:03 +0100

    test added

---
+++ src/caffe/test/test_knn_layer.cpp
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/filler.hpp"
+
+#include "caffe/test/test_caffe_main.hpp"
+
+#include "caffe/layers/knn_layer.hpp"
+
+namespace caffe {
+
+template <typename TypeParam>
+class KnnLayerTest : public MultiDeviceTest<TypeParam> {
+    typedef typename TypeParam::Dtype Dtype;
+
+protected:
+    KnnLayerTest()
+        : blob_bottom_(new Blob<Dtype>(2, 3, 4, 1))
+        , blob_bottom_2_(new Blob<Dtype>(2, 3, 4, 1))
+        , blob_top_(new Blob<Dtype>())
+    {
+        Caffe::set_random_seed(1701);
+        blob_bottom_vec_.push_back(blob_bottom_);
+        blob_bottom_vec_.push_back(blob_bottom_2_);
+        blob_top_vec_.push_back(blob_top_);
+    }
+    virtual ~KnnLayerTest()
+    {
+        delete blob_bottom_;
+        delete blob_bottom_2_;
+        delete blob_top_;
+    }
+    Blob<Dtype>* const blob_bottom_;
+    Blob<Dtype>* const blob_bottom_2_;
+    Blob<Dtype>* const blob_top_;
+};
+TYPED_TEST_CASE(KnnLayerTest, TestDtypesAndDevices);
+
+TYPED_TEST(KnnLayerTest, TestSetup)
+{
+    typedef typename TypeParam::Dtype Dtype;
+    LayerParameter layer_param;
+    int k = 3;
+    KnnParameter* knn_param = layer_param.mutable_knn_param();
+    knn_param->set_k(k);
+
+    FillerParameter filler_param;
+    filler_param.set_std(1.0);
+    GaussianFiller<Dtype> filler(filler_param);
+    filler.Fill(this->blob_bottom_);
+    filler.Fill(this->blob_bottom_2_);
+
+    shared_ptr<Layer<Dtype>> layer(
+        new KnnLayer<Dtype>(layer_param));
+
+    layer->SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+
+    EXPECT_EQ(this->blob_top_->shape(0), 2);
+    EXPECT_EQ(this->blob_top_->shape(1), k);
+    EXPECT_EQ(this->blob_top_->shape(2), this->blob_bottom_2_->shape(2));
+    EXPECT_EQ(this->blob_top_->shape(3), 1);
+}
+
+TYPED_TEST(KnnLayerTest, TestForward)
+{
+    typedef typename TypeParam::Dtype Dtype;
+    LayerParameter layer_param;
+    int k = 3;
+    KnnParameter* knn_param = layer_param.mutable_knn_param();
+    knn_param->set_k(k);
+    knn_param->set_ignore_self(false);
+
+    Dtype* ref = this->blob_bottom_->mutable_cpu_data();
+    Dtype* query = this->blob_bottom_2_->mutable_cpu_data();
+
+    for (int i = 0; i < this->blob_bottom_->count(); ++i) {
+        ref[i] = i;
+        query[i] = i;
+    }
+
+    shared_ptr<Layer<Dtype>> layer(
+        new KnnLayer<Dtype>(layer_param));
+
+    layer->SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+    layer->Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    // Now, check values
+    const int[] ans = { 0, 1, 2, 1, 0, 2, 2, 1, 3, 3, 2, 1, 0, 1, 2, 1, 0, 2, 2, 1, 3, 3, 2, 1 };
+    const Dtype* top_data = this->blob_top_->cpu_data();
+    EXPECT_EQ(top_data, ans);
+    // for (int i = 0; i < this->blob_top_->count(); ++i) {
+    //     top_data[i] = ans[i];
+    // }
+}
+
+} // namespace caffe
