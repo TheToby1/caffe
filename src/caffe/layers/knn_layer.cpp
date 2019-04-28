294b2a6202f08c7a85c3eab24c0e31bfe4cdcc8d
Toby Burns <toby8urns@gmail.com>
2019-04-28 14:02:17 +0100

    restructure

--- src/caffe/layers/knn_layer.cpp
+++ src/caffe/layers/knn_layer.cpp
@@ -12,6 +12,83 @@
 namespace caffe
 {
 
+/**
+ * Computes the Euclidean distance between a reference point and a query point.
+ *
+ * @param ref          refence points
+ * @param query        query points
+ * @param ref_index    index to the reference point to consider
+ * @param query_index  index to the query point to consider
+ * @return computed distance
+ */
+template <typename Dtype>
+float compute_distance(const Dtype *ref,
+                       const Dtype *query,
+                       int ref_index,
+                       int query_index,
+                       int ref_size,
+                       int query_size,
+                       int channels)
+{
+    Dtype sum = 0.f;
+    for (int d = 0; d < channels; ++d)
+    {
+        const Dtype diff = ref[d * ref_size + ref_index] - query[d * query_size + query_index];
+        sum += diff * diff;
+    }
+    return sqrtf(sum);
+}
+
+/**
+ * Gathers at the beginning of the `dist` array the k smallest values and their
+ * respective index (in the initial array) in the `index` array. After this call,
+ * only the k-smallest distances are available. All other distances might be lost.
+ *
+ * Since we only need to locate the k smallest distances, sorting the entire array
+ * would not be very efficient if k is relatively small. Instead, we perform a
+ * simple insertion sort by eventually inserting a given distance in the first
+ * k values.
+ *
+ * @param dist    array containing the `length` distances
+ * @param index   array containing the index of the k smallest distances
+ * @param length  total number of distances
+ */
+template <typename Dtype>
+void modified_insertion_sort(Dtype *dist, int *index, int ref_size, int k)
+{
+
+    // Initialise the first index
+    index[0] = 0;
+
+    // Go through all points
+    for (int i = 1; i < ref_size; ++i)
+    {
+
+        // Store current distance and associated index
+        Dtype curr_dist = dist[i];
+        int curr_index = i;
+        // Skip the current value if its index is > k and if it's higher the k-th already sorted smallest value
+        if (i >= k && curr_dist >= dist[k - 1])
+        {
+            continue;
+        }
+
+        // Shift values (and indexes) higher that the current distance to the right
+
+        int j = std::min(i, k - 1);
+        while (j > 0 && dist[j - 1] > curr_dist)
+        {
+            dist[j] = dist[j - 1];
+            index[j] = index[j - 1];
+            --j;
+        }
+
+        // Write the current distance and index at their position
+        dist[j] = curr_dist;
+        index[j] = curr_index;
+    }
+}
+
 template <typename Dtype>
 void KnnLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype> *> &bottom,
                                  const vector<Blob<Dtype> *> &top)
@@ -42,7 +119,7 @@ void KnnLayer<Dtype>::Reshape(const vector<Blob<Dtype> *> &bottom,
     ref_size_ = bottom[0]->shape(axis_);
     query_size_ = bottom[1]->shape(axis_);
 
-    this->blobs_[0].reset(new Blob<Dtype>(bottom[1]->shape[0], 1, bottom[0]->shape(axis_), bottom[1]->shape(axis_)));
+    this->blobs_[0].reset(new Blob<Dtype>(bottom[1]->shape(0), 1, bottom[0]->shape(axis_), bottom[1]->shape(axis_)));
 }
 
 template <typename Dtype>
@@ -68,86 +145,12 @@ void KnnLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype> *> &bottom,
             // Compute all distances / indexes
             for (int j = 0; j < ref_size_; ++j)
             {
-                dist[j] = compute_distance(cur_ref, cur_query, j, i);
+                dist[j] = compute_distance(cur_ref, cur_query, j, i, ref_size_, query_size_, channels_);
             }
 
             // Sort distances / indexes
-            modified_insertion_sort(dist, k_index + b * k_);
+            modified_insertion_sort(dist, k_index + b * k_, ref_size, k_);
         }
-    }
-}
-
-/**
- * Computes the Euclidean distance between a reference point and a query point.
- *
- * @param ref          refence points
- * @param query        query points
- * @param ref_index    index to the reference point to consider
- * @param query_index  index to the query point to consider
- * @return computed distance
- */
-template <typename Dtype>
-float compute_distance(const Dtype *ref,
-                       const Dtype *query,
-                       int ref_index,
-                       int query_index)
-{
-    Dtype sum = 0.f;
-    for (int d = 0; d < channels_; ++d)
-    {
-        const Dtype diff = ref[d * ref_size_ + ref_index] - query[d * query_size_ + query_index];
-        sum += diff * diff;
-    }
-    return sqrtf(sum);
-}
-
-/**
- * Gathers at the beginning of the `dist` array the k smallest values and their
- * respective index (in the initial array) in the `index` array. After this call,
- * only the k-smallest distances are available. All other distances might be lost.
- *
- * Since we only need to locate the k smallest distances, sorting the entire array
- * would not be very efficient if k is relatively small. Instead, we perform a
- * simple insertion sort by eventually inserting a given distance in the first
- * k values.
- *
- * @param dist    array containing the `length` distances
- * @param index   array containing the index of the k smallest distances
- * @param length  total number of distances
- */
-template <typename Dtype>
-void modified_insertion_sort(Dtype *dist, int *index)
-{
-
-    // Initialise the first index
-    index[0] = 0;
-
-    // Go through all points
-    for (int i = 1; i < ref_size_; ++i)
-    {
-
-        // Store current distance and associated index
-        Dtype curr_dist = dist[i];
-        int curr_index = i;
-        // Skip the current value if its index is > k and if it's higher the k-th already sorted smallest value
-        if (i >= k_ && curr_dist >= dist[k_ - 1])
-        {
-            continue;
-        }
-
-        // Shift values (and indexes) higher that the current distance to the right
-
-        int j = std::min(i, k_ - 1);
-        while (j > 0 && dist[j - 1] > curr_dist)
-        {
-            dist[j] = dist[j - 1];
-            index[j] = index[j - 1];
-            --j;
-        }
-
-        // Write the current distance and index at their position
-        dist[j] = curr_dist;
-        index[j] = curr_index;
     }
 }
 
