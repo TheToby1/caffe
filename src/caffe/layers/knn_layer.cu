ea9119856c5117a6e40b6446aeb6cfad032bc75b
Toby Burns <toby8urns@gmail.com>
2019-04-28 21:16:08 +0100

    type fix

--- src/caffe/layers/knn_layer.cu
+++ src/caffe/layers/knn_layer.cu
@@ -159,17 +159,19 @@ void KnnLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& bottom, const vec
     // memory in blobs must be the same";
     const int count = this->blobs_[0]->count();
 
-    if (propagate_down[0]) {
+        if (propagate_down[0]) {
+        Dtype fill = 0;
         Dtype* ref_diff = bottom[0]->mutable_gpu_diff();
-        caffe_gpu_set(bottom[0]->count(), 0., ref_diff);
+        caffe_gpu_set(bottom[0]->count(), fill, ref_diff);
         compute_ref_diff<Dtype> // NOLINT_NEXT_LINE(whitespace/operators)
             <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
                 count, ref_data, query_data, ref_size_,
                 query_size_, channels_, k_, ref_diff);
     }
     if (propagate_down[1]) {
+        Dtype fill = 0;
         Dtype* query_diff = bottom[1]->mutable_gpu_diff();
-        caffe_gpu_set(bottom[1]->count(), 0., query_diff);
+        caffe_gpu_set(bottom[1]->count(), fill, query_diff);
         compute_query_diff<Dtype> // NOLINT_NEXT_LINE(whitespace/operators)
             <<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
                 count, ref_data, query_data, ref_size_,
